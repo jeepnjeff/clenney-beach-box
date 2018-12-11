5d1679f3743a0b28eb8f4b33151a00b94684ed48
jeepnjeff <jeffreypendergrass@hotmail.com>
2018-12-11 09:53:09 -0800

    added dark background and css upgrades

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 //   Gallery
 
-/* show lightbox when clicking a thumbnail */
-$('a.thumb').click(function(event) {
-    event.preventDefault();
-    var content = $('.modal-body');
-    content.empty();
-    var title = $(this).attr("title");
-    $('.modal-title').html(title);
-    content.html($(this).html());
-    $(".modal-profile").modal({ show: true });
-});
+// /* show lightbox when clicking a thumbnail */
+// $('a.thumb').click(function(event) {
+//     event.preventDefault();
+//     var content = $('.modal-body');
+//     content.empty();
+//     var title = $(this).attr("title");
+//     $('.modal-title').html(title);
+//     content.html($(this).html());
+//     $(".modal-profile").modal({ show: true });
+// });
 // End Gallery
 
 // Modal
